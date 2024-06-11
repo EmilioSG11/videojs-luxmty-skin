2c83d04231c261eaf3198915b8e91057b13af812
EmilioSG11 <emiliosalasgzz@gmail.com>
2024-06-11 15:13:14 -0600

    update

--- dist/vjs-luxmty.js
+++ dist/vjs-luxmty.js
@@ -1,7 +1,12 @@
-var player = videojs("luxmty-player", {
+var player = videojs(document.querySelector('video-js'), {
   controlBar: {
-    children: ['playToggle', 'skipBackward', 'skipForward', 'volumePanel', 'currentTimeDisplay', 'timeDivider', 'durationDisplay', 'progressControl', 'liveDisplay', 'seekToLive', 'remainingTimeDisplay', 'customControlSpacer', 'playbackRateMenuButton', 'chaptersButton', 'descriptionsButton', 'subsCapsButton', 'audioTrackButton', 'ShareButton', 'pictureInPictureToggle', 'fullscreenToggle'],
+    children: ['playToggle', 'skipBackward', 'skipForward', 'volumePanel', 'currentTimeDisplay', 'timeDivider', 'durationDisplay', 'progressControl', 'liveDisplay', 'seekToLive', 'remainingTimeDisplay', 'customControlSpacer', 'playbackRateMenuButton', 'chaptersButton', 'descriptionsButton', 'subsCapsButton', 'audioTrackButton', 'ShareButton', 'hlsQualitySelector', 'QualitySelector', 'pictureInPictureToggle', 'fullscreenToggle'],
+    skipButtons: {
+      forward: 10,
+      backward: 10
+    },
   },
+  inactivityTimeout: 3000,
   preload: "metadata",
   autoplay: "", //for autoplay "muted"
   controls: true,
@@ -9,13 +14,10 @@ var player = videojs("luxmty-player", {
   fluid: true,
   liveui: true,
   language: '',
+  playbackRates: [0.25, 0.5, 0.75, 1, 1.25, 1.5, 2],
+  poster: 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAACklEQVR4nGMAAQAABQABDQottAAAAABJRU5ErkJggg==',
+  techOrder: ["html5"],
   html5: {
-    vhs: {
-      enableLowInitialPlaylist: true,
-      fastQualityChange: true,
-      overrideNative: true,
-      useDevicePixelRatio: false,
-      limitRenditionByPlayerDimensions: false,
-    }
   },
 });
+
